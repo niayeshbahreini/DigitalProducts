c10fbd186e9eeafdf532f75cdf0a8a405332417b
niayesh <niayeshbhrn@gmail.com>
2022-09-20 18:15:41 +0430

    apps added.

--- digitalProducts/urls.py
+++ digitalProducts/urls.py
@@ -1,4 +1,4 @@
-"""digitalProducts URL Configuration
+"""digital_products URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('products.urls')),
+    path('', include('users.urls'))
 ]
+
+if settings.IS_DEVEL:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
